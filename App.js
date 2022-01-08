fc25d8774104abfd1a93584c8832470aceb7e65b
rahuldubey99 <saravanan.angler@kofluence.com>
2022-01-08 10:15:42 +0530

    add token 30 may 22

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// git project token
+// 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
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from "./screens/Home";
